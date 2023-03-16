7eaee8b70242ca999ff4111efcb86b6c57314016
Isabelle Johannesson <isabelle.johannesson@outlook.com>
2023-03-16 22:20:40 +0100

    skrivit ut kontaktinfo, kommit en bit med styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { ContactinfoComponent } from './contactinfo/contactinfo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactinfoComponent,
   ],
   imports: [
     BrowserModule
